441ccb1c8eaddc5021cc2f139f6ad1aa85539cd8
Tan Chang Kiat <tanchangkiat@gmail.com>
2022-04-12 18:50:44 +0800

    Initial commit

---
+++ test/aws-containers-tfc-work.test.js
@@ -0,0 +1,17 @@
+// const cdk = require('aws-cdk-lib');
+// const { Template } = require('aws-cdk-lib/assertions');
+// const AwsCtnTfcWork = require('../lib/aws-containers-tfc-work-stack');
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/aws-containers-tfc-work-stack.js
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//   // WHEN
+//   const stack = new AwsCtnTfcWork.AwsCtnTfcWorkStack(app, 'MyTestStack');
+//   // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
