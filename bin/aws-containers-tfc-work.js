2d742e2ed45929b132d259d6dfefb160d886de7b
Tan Chang Kiat <tanchangkiat@gmail.com>
2022-04-22 14:18:42 +0800

    Clean-up code and added a new template to create a standard VPC

--- bin/aws-containers-tfc-work.js
+++ bin/aws-containers-tfc-work.js
@@ -2,25 +2,19 @@
 require('dotenv').config();
 
 const cdk = require('aws-cdk-lib');
+const { StandardVpc } = require('../lib/StandardVpc');
 const { Homework1 } = require('../lib/Homework1');
 const { Homework2 } = require('../lib/Homework2');
 const { MultiArchPipeline } = require('../lib/MultiArchPipeline');
 
 const app = new cdk.App();
-new Homework1(app, 'hw1', {
-  /* If you don't specify 'env', this stack will be environment-agnostic.
-   * Account/Region-dependent features and context lookups will not work,
-   * but a single synthesized template can be deployed anywhere. */
 
-  /* Uncomment the next line to specialize this stack for the AWS Account
-   * and Region that are implied by the current CLI configuration. */
+new StandardVpc(app, 'vpc', {
   env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
+});
 
-  /* Uncomment the next line if you know exactly what Account and Region you
-   * want to deploy the stack to. */
-  // env: { account: '123456789012', region: 'us-east-1' },
-
-  /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
+new Homework1(app, 'hw1', {
+  env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
 });
 
 new Homework2(app, 'hw2', {
