d61eae11368fce49236a4d6621554399c31320a3
Tan Chang Kiat <tanchangkiat@gmail.com>
2022-04-28 11:24:11 +0800

    Added homework 3

--- bin/aws-containers-tfc-work.js
+++ bin/aws-containers-tfc-work.js
@@ -5,6 +5,7 @@ const cdk = require('aws-cdk-lib');
 const { StandardVpc } = require('../lib/StandardVpc');
 const { Homework1 } = require('../lib/Homework1');
 const { Homework2 } = require('../lib/Homework2');
+const { Homework3 } = require('../lib/Homework3');
 const { MultiArchPipeline } = require('../lib/MultiArchPipeline');
 
 const app = new cdk.App();
@@ -25,4 +26,8 @@ new Homework2(app, 'hw2', {
 new MultiArchPipeline(app, 'mapl', {
   // github_connection_arn: Go to https://console.aws.amazon.com/codesuite/settings/connections to set up a connection to GitHub, fill up the ARN in .env
   env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION, github_connection_arn: process.env.CDK_GITHUB_CONNECTION_ARN, github_owner: process.env.CDK_GITHUB_OWNER, github_repo: process.env.CDK_GITHUB_REPO },
+});
+
+new Homework3(app, 'hw3', {
+  env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
 });
