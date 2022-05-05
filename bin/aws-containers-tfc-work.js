bcfb87612b7104ed345a0cae6a47b6d60b12ba88
Tan Chang Kiat <tanchangkiat@gmail.com>
2022-05-05 14:40:54 +0800

    Added a template for creation of EC2 fleet and CICD pipeline

--- bin/aws-containers-tfc-work.js
+++ bin/aws-containers-tfc-work.js
@@ -1,38 +1,70 @@
 #!/usr/bin/env node
-require('dotenv').config();
+require("dotenv").config();
 
-const cdk = require('aws-cdk-lib');
-const { StandardVpc } = require('../lib/StandardVpc');
-const { Homework1 } = require('../lib/Homework1');
-const { Homework2 } = require('../lib/Homework2');
-const { EKS } = require('../lib/EKS');
-const { AdotEcsFargate } = require('../lib/AdotEcsFargate');
-const { MultiArchPipeline } = require('../lib/MultiArchPipeline');
+const cdk = require("aws-cdk-lib");
+const { StandardVpc } = require("../lib/StandardVpc");
+const { Homework1 } = require("../lib/Homework1");
+const { Homework2 } = require("../lib/Homework2");
+const { EKS } = require("../lib/EKS");
+const { AdotEcsFargate } = require("../lib/AdotEcsFargate");
+const { MultiArchPipeline } = require("../lib/MultiArchPipeline");
+const { CicdEc2 } = require("../lib/CICD-EC2");
 
 const app = new cdk.App();
 
-new StandardVpc(app, 'vpc', {
-  env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
+new StandardVpc(app, "vpc", {
+  env: {
+    account: process.env.CDK_DEFAULT_ACCOUNT,
+    region: process.env.CDK_DEFAULT_REGION,
+  },
 });
 
-new Homework1(app, 'hw1', {
-  env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
+new Homework1(app, "hw1", {
+  env: {
+    account: process.env.CDK_DEFAULT_ACCOUNT,
+    region: process.env.CDK_DEFAULT_REGION,
+  },
 });
 
-new Homework2(app, 'hw2', {
+new Homework2(app, "hw2", {
   // github_connection_arn: Go to https://console.aws.amazon.com/codesuite/settings/connections to set up a connection to GitHub, fill up the ARN in .env
-  env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION, github_connection_arn: process.env.CDK_GITHUB_CONNECTION_ARN, github_owner: process.env.CDK_GITHUB_OWNER, github_repo: process.env.CDK_GITHUB_REPO },
+  env: {
+    account: process.env.CDK_DEFAULT_ACCOUNT,
+    region: process.env.CDK_DEFAULT_REGION,
+    github_connection_arn: process.env.CDK_GITHUB_CONNECTION_ARN,
+    github_owner: process.env.CDK_GITHUB_OWNER,
+    github_repo: process.env.CDK_GITHUB_REPO,
+  },
 });
 
-new MultiArchPipeline(app, 'mapl', {
+new MultiArchPipeline(app, "mapl", {
   // github_connection_arn: Go to https://console.aws.amazon.com/codesuite/settings/connections to set up a connection to GitHub, fill up the ARN in .env
-  env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION, github_connection_arn: process.env.CDK_GITHUB_CONNECTION_ARN, github_owner: process.env.CDK_GITHUB_OWNER, github_repo: process.env.CDK_GITHUB_REPO },
+  env: {
+    account: process.env.CDK_DEFAULT_ACCOUNT,
+    region: process.env.CDK_DEFAULT_REGION,
+    github_connection_arn: process.env.CDK_GITHUB_CONNECTION_ARN,
+    github_owner: process.env.CDK_GITHUB_OWNER,
+    github_repo: process.env.CDK_GITHUB_REPO,
+  },
 });
 
-new EKS(app, 'eks-cluster', {
-  env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
+new EKS(app, "eks-cluster", {
+  env: {
+    account: process.env.CDK_DEFAULT_ACCOUNT,
+    region: process.env.CDK_DEFAULT_REGION,
+  },
 });
 
-new AdotEcsFargate(app, 'adot-ecs', {
-  env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
+new AdotEcsFargate(app, "adot-ecs", {
+  env: {
+    account: process.env.CDK_DEFAULT_ACCOUNT,
+    region: process.env.CDK_DEFAULT_REGION,
+  },
+});
+
+new CicdEc2(app, "cicd-ec2", {
+  env: {
+    account: process.env.CDK_DEFAULT_ACCOUNT,
+    region: process.env.CDK_DEFAULT_REGION,
+  },
 });
