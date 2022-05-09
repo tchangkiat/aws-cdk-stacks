9912840cf1ef3c49005308a5146ee3f200424ef3
Tan Chang Kiat <tanchangkiat@gmail.com>
2022-05-09 23:09:06 +0800

    Added a script to set up bastion host for EKS cluster

---
+++ scripts/EKS/setup-bastion-host.sh
@@ -0,0 +1,16 @@
+aws configure set region $AWS_REGION
+aws configure set output json
+
+aws eks update-kubeconfig --name $AWS_EKS_CLUSTER --region $AWS_REGION --role-arn $AWS_EKS_CLUSTER_MASTER_ROLE
+
+# Add ARN of the caller identity (AWS CLI) to configmap/aws-auth so that there will not be any error executing commands like `eksctl create iamserviceaccount`
+
+export AWS_IAM_USER_ARN=`aws sts get-caller-identity --query Arn --output text`
+
+export AWS_AUTH_MAP_USERS="mapUsers: '[{\"userarn\":\"$AWS_IAM_USER_ARN\",\"username\":\"admin\",\"groups\":[\"system:masters\"]}]'"
+
+kubectl get -n kube-system configmap/aws-auth -o yaml > aws-auth.yml
+
+sed "s|mapUsers: '\[\]'|$AWS_AUTH_MAP_USERS|g" aws-auth.yml > aws-auth-patched.yml
+
+kubectl apply -f aws-auth-patched.yml
