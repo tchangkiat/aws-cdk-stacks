38b00ba2528fa5ef417ccd5b903d34d0010957ed
Tan Chang Kiat <47205264+tchangkiat@users.noreply.github.com>
2022-09-26 22:09:17 +0800

    Update install-app-mesh-controller.sh

--- scripts/EKS/install-app-mesh-controller.sh
+++ scripts/EKS/install-app-mesh-controller.sh
@@ -10,4 +10,6 @@ kubectl apply -k "https://github.com/aws/eks-charts/stable/appmesh-controller/cr
 
 eksctl create iamserviceaccount --namespace appmesh-system --name appmesh-controller --attach-policy-arn arn:aws:iam::aws:policy/AWSCloudMapFullAccess,arn:aws:iam::aws:policy/AWSAppMeshFullAccess,arn:aws:iam::aws:policy/AWSXRayDaemonWriteAccess --cluster $AWS_EKS_CLUSTER --approve
 
+helm repo update
+
 helm upgrade -i appmesh-controller eks/appmesh-controller --namespace appmesh-system --set region=$AWS_REGION --set serviceAccount.create=false --set serviceAccount.name=appmesh-controller
