a17f042c5809c655077dfb9b161738fe6a330f5e
Tan Chang Kiat <tanchangkiat@gmail.com>
2023-09-18 21:32:27 +0800

    Minor change and clean up files

--- scripts/EKS/install-ray.sh
+++ scripts/EKS/install-ray.sh
@@ -128,7 +128,6 @@ helm install kuberay-operator kuberay/kuberay-operator --version 0.6.0
 # Install a RayCluster custom resource
 helm install raycluster kuberay/ray-cluster --version 0.6.0 --values ray-cluster-config.yaml
 
-echo "Sleep for 80 seconds - waiting for Ray pods to start"
-sleep 80
+echo "Installation completed. Pods may take a few minutes to start."
 
 echo "Access Ray Dashboard by running this command 'kubectl port-forward --address 0.0.0.0 svc/raycluster-kuberay-head-svc 8265:8265' and access 'http://localhost:8265' on your client machine"
