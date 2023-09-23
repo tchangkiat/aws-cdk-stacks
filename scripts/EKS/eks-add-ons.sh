c0cde4fbc4ef250990ab85f8a676e438e428492c
Tan Chang Kiat <tanchangkiat@gmail.com>
2023-09-23 10:46:21 +0800

    Improved EKS add-ons script

--- scripts/EKS/eks-add-ons.sh
+++ scripts/EKS/eks-add-ons.sh
@@ -11,81 +11,87 @@ done
 shift $((OPTIND -1))
 
 for install in "${installs[@]}"; do
-    case $install in
-        "1"|"karpenter")
-            scripts+=("install-karpenter.sh")
-            ;;
-        "2"|"load-balancer-controller")
-            scripts+=("install-load-balancer-controller.sh")
-            ;;
-        "3"|"ebs-csi-driver")
-            scripts+=("install-ebs-csi-driver.sh")
-            ;;
-        "4"|"container-insights")
-            scripts+=("install-container-insights.sh")
-            ;;
-        "5"|"prometheus-grafana")
-            scripts+=("install-prometheus-grafana.sh")
-            ;;
-        "6"|"ingress-nginx-controller")
-            scripts+=("install-ingress-nginx-controller.sh")
-            ;;
-        "7"|"app-mesh-controller")
-            scripts+=("install-app-mesh-controller.sh")
-            ;;
-        "8"|"gateway-api-controller")
-            scripts+=("install-gateway-api-controller.sh")
-            ;;
-        "9"|"emr-on-eks")
-            scripts+=("setup-emr-on-eks.sh")
-            ;;
-        "10"|"jupyterhub")
-            scripts+=("install-jupyterhub.sh")
-            ;;
-        "11"|"ray")
-            scripts+=("install-ray.sh")
-            ;;
-        *) echo "Invalid option $REPLY"
-    esac
+    for add_on_id_or_alias in $install
+    do
+        case $add_on_id_or_alias in
+            "1"|"karpenter")
+                scripts+=("install-karpenter.sh")
+                ;;
+            "2"|"load-balancer-controller")
+                scripts+=("install-load-balancer-controller.sh")
+                ;;
+            "3"|"ebs-csi-driver")
+                scripts+=("install-ebs-csi-driver.sh")
+                ;;
+            "4"|"container-insights")
+                scripts+=("install-container-insights.sh")
+                ;;
+            "5"|"prometheus-grafana")
+                scripts+=("install-prometheus-grafana.sh")
+                ;;
+            "6"|"ingress-nginx-controller")
+                scripts+=("install-ingress-nginx-controller.sh")
+                ;;
+            "7"|"app-mesh-controller")
+                scripts+=("install-app-mesh-controller.sh")
+                ;;
+            "8"|"gateway-api-controller")
+                scripts+=("install-gateway-api-controller.sh")
+                ;;
+            "9"|"emr-on-eks")
+                scripts+=("setup-emr-on-eks.sh")
+                ;;
+            "10"|"jupyterhub")
+                scripts+=("install-jupyterhub.sh")
+                ;;
+            "11"|"ray")
+                scripts+=("install-ray.sh")
+                ;;
+            *) echo "Invalid option $REPLY"
+        esac
+    done
 done
 
 for removal in "${removals[@]}"; do
-    case $removal in
-        "1"|"karpenter")
-            scripts+=("remove-karpenter.sh")
-            ;;
-        "2"|"load-balancer-controller")
-            scripts+=("remove-load-balancer-controller.sh")
-            ;;
-        "3"|"ebs-csi-driver")
-            scripts+=("remove-ebs-csi-driver.sh")
-            ;;
-        "4"|"container-insights")
-            scripts+=("remove-container-insights.sh")
-            ;;
-        "5"|"prometheus-grafana")
-            scripts+=("remove-prometheus-grafana.sh")
-            ;;
-        "6"|"ingress-nginx-controller")
-            scripts+=("remove-ingress-nginx-controller.sh")
-            ;;
-        "7"|"app-mesh-controller")
-            scripts+=("remove-app-mesh-controller.sh")
-            ;;
-        "8"|"gateway-api-controller")
-            scripts+=("remove-gateway-api-controller.sh")
-            ;;
-        "9"|"emr-on-eks")
-            scripts+=("remove-emr-on-eks.sh")
-            ;;
-        "10"|"jupyterhub")
-            scripts+=("remove-jupyterhub.sh")
-            ;;
-        "11"|"ray")
-            scripts+=("remove-ray.sh")
-            ;;
-        *) echo "Invalid option $REPLY"
-    esac
+    for add_on_id_or_alias in $removal
+    do
+        case $add_on_id_or_alias in
+            "1"|"karpenter")
+                scripts+=("remove-karpenter.sh")
+                ;;
+            "2"|"load-balancer-controller")
+                scripts+=("remove-load-balancer-controller.sh")
+                ;;
+            "3"|"ebs-csi-driver")
+                scripts+=("remove-ebs-csi-driver.sh")
+                ;;
+            "4"|"container-insights")
+                scripts+=("remove-container-insights.sh")
+                ;;
+            "5"|"prometheus-grafana")
+                scripts+=("remove-prometheus-grafana.sh")
+                ;;
+            "6"|"ingress-nginx-controller")
+                scripts+=("remove-ingress-nginx-controller.sh")
+                ;;
+            "7"|"app-mesh-controller")
+                scripts+=("remove-app-mesh-controller.sh")
+                ;;
+            "8"|"gateway-api-controller")
+                scripts+=("remove-gateway-api-controller.sh")
+                ;;
+            "9"|"emr-on-eks")
+                scripts+=("remove-emr-on-eks.sh")
+                ;;
+            "10"|"jupyterhub")
+                scripts+=("remove-jupyterhub.sh")
+                ;;
+            "11"|"ray")
+                scripts+=("remove-ray.sh")
+                ;;
+            *) echo "Invalid option $REPLY"
+        esac
+    done
 done
 
 if [[ ${#scripts[@]} -ne 0 ]]
