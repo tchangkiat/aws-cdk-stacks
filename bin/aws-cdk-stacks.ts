a275f7269dc7cbcb34b45c89447091d37cdad63c
Tan Chang Kiat <tanchangkiat@gmail.com>
2024-01-10 11:13:24 +0800

    Added a prefix for CloudFormation stack names

--- bin/aws-cdk-stacks.ts
+++ bin/aws-cdk-stacks.ts
@@ -18,23 +18,24 @@ import { Autoscaler } from '../constants'
 dotenv.config()
 
 const app = new cdk.App()
+const prefix = 'cdk-stacks-'
 
-const common = new Common(app, 'aws-cdk-stacks-common')
+const common = new Common(app, 'common', { stackName: prefix + 'common' })
 
-const multiArchPipeline = new MultiArchPipeline(app, 'multi-arch-pipeline', common.GitHub)
+const multiArchPipeline = new MultiArchPipeline(app, 'multi-arch-pipeline', common.GitHub, { stackName: prefix + 'multi-arch-pipeline' })
 
-const ecs = new ECS(app, 'ecs', common.Vpc, multiArchPipeline.Repository)
+const ecs = new ECS(app, 'ecs', common.Vpc, multiArchPipeline.Repository, { stackName: prefix + 'ecs' })
 
-new EcsCicd(app, 'ecs-cicd', ecs.FargateService, multiArchPipeline.Repository, common.GitHub)
+new EcsCicd(app, 'ecs-cicd', ecs.FargateService, multiArchPipeline.Repository, common.GitHub, { stackName: prefix + 'ecs-cicd' })
 
-new EKS(app, 'eks', common.Vpc, multiArchPipeline.Repository)
+new EKS(app, 'eks', common.Vpc, multiArchPipeline.Repository, undefined, { stackName: prefix + 'eks' })
 
-new EKS(app, 'eks-ca', common.Vpc, multiArchPipeline.Repository, Autoscaler.ClusterAutoscaler)
+new EKS(app, 'eks-ca', common.Vpc, multiArchPipeline.Repository, Autoscaler.ClusterAutoscaler, { stackName: prefix + 'eks-ca' })
 
-new EgressVpc(app, 'egress-vpc')
+new EgressVpc(app, 'egress-vpc', { stackName: prefix + 'egress-vpc' })
 
-new ALBRuleRestriction(app, 'alb-rule-restriction')
+new ALBRuleRestriction(app, 'alb-rule-restriction', { stackName: prefix + 'alb-rule-restriction' })
 
-new ApiGateway(app, 'api-gateway', common.Vpc)
+new ApiGateway(app, 'api-gateway', common.Vpc, { stackName: prefix + 'api-gateway' })
 
-new PostgresDatabase(app, 'postgres-db', common.Vpc)
+new PostgresDatabase(app, 'postgres-db', common.Vpc, { stackName: prefix + 'postgresql' })
