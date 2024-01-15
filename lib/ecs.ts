611ed4f5c2e4b012413742156601bfd66100e730
Tan Chang Kiat <tanchangkiat@gmail.com>
2024-01-15 14:01:56 +0800

    Updated ECS stack

--- lib/ecs.ts
+++ lib/ecs.ts
@@ -1,11 +1,11 @@
 import { type Construct } from 'constructs'
-import { Stack, type StackProps, Duration, RemovalPolicy } from 'aws-cdk-lib'
-import type * as ec2 from 'aws-cdk-lib/aws-ec2'
+import { Stack, type StackProps, Duration, RemovalPolicy, CfnOutput } from 'aws-cdk-lib'
+import * as ec2 from 'aws-cdk-lib/aws-ec2'
 import * as ecs from 'aws-cdk-lib/aws-ecs'
 import * as ecsPatterns from 'aws-cdk-lib/aws-ecs-patterns'
-// import * as autoscaling from 'aws-cdk-lib/aws-autoscaling'
+import * as autoscaling from 'aws-cdk-lib/aws-autoscaling'
 import * as cloudwatch from 'aws-cdk-lib/aws-cloudwatch'
-// import * as elbv2 from 'aws-cdk-lib/aws-elasticloadbalancingv2'
+import * as elbv2 from 'aws-cdk-lib/aws-elasticloadbalancingv2'
 import * as iam from 'aws-cdk-lib/aws-iam'
 import type * as ecr from 'aws-cdk-lib/aws-ecr'
 import * as logs from 'aws-cdk-lib/aws-logs'
@@ -85,89 +85,90 @@ export class ECS extends Stack {
     // ECS Cluster > EC2 Capacity Provider
     // ----------------------------
 
-    // const asg = new autoscaling.AutoScalingGroup(this, 'asg', {
-    //   vpc,
-    //   autoScalingGroupName: prefix + '-asg',
-    //   instanceType: new ec2.InstanceType('t3.medium'),
-    //   machineImage: ecs.EcsOptimizedImage.amazonLinux2023(),
-    //   minCapacity: 1,
-    //   maxCapacity: 5
-    // })
-    // asg.scaleOnCpuUtilization('asg-cpu-scaling', {
-    //   targetUtilizationPercent: 70
-    // })
-    // const ec2CapacityProvider = new ecs.AsgCapacityProvider(
-    //   this,
-    //   'ec2-asg-capacity-provider',
-    //   {
-    //     autoScalingGroup: asg,
-    //     capacityProviderName: 'ec2'
-    //   }
-    // )
-    // this.Cluster.addAsgCapacityProvider(ec2CapacityProvider)
+    const asg = new autoscaling.AutoScalingGroup(this, 'asg', {
+      vpc,
+      autoScalingGroupName: prefix + '-asg',
+      instanceType: new ec2.InstanceType('t3.medium'),
+      machineImage: ecs.EcsOptimizedImage.amazonLinux2023(),
+      minCapacity: 1,
+      maxCapacity: 5
+    })
+    asg.scaleOnCpuUtilization('asg-cpu-scaling', {
+      targetUtilizationPercent: 70
+    })
+    const ec2CapacityProvider = new ecs.AsgCapacityProvider(
+      this,
+      'ec2-asg-capacity-provider',
+      {
+        autoScalingGroup: asg,
+        capacityProviderName: 'ec2'
+      }
+    )
+    this.Cluster.addAsgCapacityProvider(ec2CapacityProvider)
 
-    // // Task Definition
-    // const ec2TaskDefinition = new ecs.Ec2TaskDefinition(
-    //   this,
-    //   'Ec2TaskDefinition',
-    //   { networkMode: ecs.NetworkMode.AWS_VPC }
-    // )
-    // ec2TaskDefinition.addContainer('ec2-app', {
-    //   image: ecs.ContainerImage.fromEcrRepository(repository),
-    //   containerName: 'sample-express-api',
-    //   portMappings: [{ containerPort: 8000 }],
-    //   cpu: 256,
-    //   memoryLimitMiB: 512,
-    //   logging: ecs.LogDrivers.firelens({
-    //     options: {
-    //       Name: 'cloudwatch',
-    //       region: 'ap-southeast-1',
-    //       log_group_name: '/aws/ecs/containerinsights/' + prefix + '/application',
-    //       auto_create_group: 'true',
-    //       log_stream_name: 'sample-express-api-$(ecs_task_id)',
-    //       retry_limit: '2'
-    //     }
-    //   })
-    // })
+    // Task Definition
+    const ec2TaskDefinition = new ecs.Ec2TaskDefinition(
+      this,
+      'Ec2TaskDefinition',
+      { networkMode: ecs.NetworkMode.AWS_VPC }
+    )
+    ec2TaskDefinition.addContainer('ec2-app', {
+      image: ecs.ContainerImage.fromEcrRepository(repository),
+      containerName: 'sample-express-api',
+      portMappings: [{ containerPort: 8000 }],
+      cpu: 256,
+      memoryLimitMiB: 512,
+      logging: ecs.LogDrivers.firelens({
+        options: {
+          Name: 'cloudwatch',
+          region: 'ap-southeast-1',
+          log_group_name: '/aws/ecs/containerinsights/' + prefix + '/application',
+          auto_create_group: 'true',
+          log_stream_name: 'sample-express-api-$(ecs_task_id)',
+          retry_limit: '2'
+        }
+      })
+    })
 
-    // const ec2Service = new ecs.Ec2Service(this, 'Ec2Service', {
-    //   cluster: this.Cluster,
-    //   taskDefinition: ec2TaskDefinition,
-    //   serviceName: prefix + '-ec2-service',
-    //   desiredCount: 1,
-    //   placementStrategies: [
-    //     ecs.PlacementStrategy.spreadAcrossInstances(),
-    //     ecs.PlacementStrategy.packedByCpu()
-    //   ],
-    //   capacityProviderStrategies: [
-    //     {
-    //       capacityProvider: ec2CapacityProvider.capacityProviderName,
-    //       base: 1,
-    //       weight: 1
-    //     }
-    //   ]
-    // })
+    const ec2Service = new ecs.Ec2Service(this, 'Ec2Service', {
+      cluster: this.Cluster,
+      taskDefinition: ec2TaskDefinition,
+      serviceName: prefix + '-ec2-service',
+      desiredCount: 1,
+      enableECSManagedTags: true,
+      placementStrategies: [
+        ecs.PlacementStrategy.spreadAcrossInstances(),
+        ecs.PlacementStrategy.packedByCpu()
+      ],
+      capacityProviderStrategies: [
+        {
+          capacityProvider: ec2CapacityProvider.capacityProviderName,
+          base: 1,
+          weight: 1
+        }
+      ]
+    })
 
-    // const lb = new elbv2.ApplicationLoadBalancer(this, 'Ec2ServiceAlb', {
-    //   vpc,
-    //   internetFacing: true
-    // })
-    // const listener = lb.addListener('Ec2ServiceAlbListener', {
-    //   port: 80
-    // })
-    // listener.addTargets('Ec2ServiceAlbTarget', {
-    //   port: 80,
-    //   targets: [
-    //     ec2Service.loadBalancerTarget({
-    //       containerName: 'sample-express-api',
-    //       containerPort: 8000
-    //     })
-    //   ]
-    // })
+    const lb = new elbv2.ApplicationLoadBalancer(this, 'Ec2ServiceAlb', {
+      vpc,
+      internetFacing: true
+    })
+    const listener = lb.addListener('Ec2ServiceAlbListener', {
+      port: 80
+    })
+    listener.addTargets('Ec2ServiceAlbTarget', {
+      port: 80,
+      targets: [
+        ec2Service.loadBalancerTarget({
+          containerName: 'sample-express-api',
+          containerPort: 8000
+        })
+      ]
+    })
 
-    // new CfnOutput(this, 'EC2 Service Load Balancer DNS', {
-    //   value: 'http://' + lb.loadBalancerDnsName
-    // })
+    new CfnOutput(this, 'EC2 Service Load Balancer DNS', {
+      value: 'http://' + lb.loadBalancerDnsName
+    })
 
     // ----------------------------
     // ECS Cluster > Fargate
@@ -226,7 +227,8 @@ export class ECS extends Stack {
           publicLoadBalancer: true,
           serviceName: prefix + '-fg-service',
           loadBalancerName: prefix + '-fg-service-lb',
-          taskDefinition: fgTaskDef
+          taskDefinition: fgTaskDef,
+          enableECSManagedTags: true
         }
       ).service
     const fargateServiceScalability = this.FargateService.autoScaleTaskCount({ maxCapacity: 3 })
@@ -245,10 +247,10 @@ export class ECS extends Stack {
       dashboardName: prefix
     })
 
-    // const ec2CpuUtilizationMetric = ec2Service.metricCpuUtilization({
-    //   period: Duration.minutes(1),
-    //   label: 'EC2 CPU Utilization'
-    // })
+    const ec2CpuUtilizationMetric = ec2Service.metricCpuUtilization({
+      period: Duration.minutes(1),
+      label: 'EC2 CPU Utilization'
+    })
 
     const fargateCpuUtilizationMetric = this.FargateService.metricCpuUtilization({
       period: Duration.minutes(1),
@@ -256,11 +258,11 @@ export class ECS extends Stack {
     })
 
     dashboard.addWidgets(
-      // new cloudwatch.GraphWidget({
-      //   left: [ec2CpuUtilizationMetric],
-      //   width: 12,
-      //   title: 'EC2 CPU Utilization'
-      // }),
+      new cloudwatch.GraphWidget({
+        left: [ec2CpuUtilizationMetric],
+        width: 12,
+        title: 'EC2 CPU Utilization'
+      }),
       new cloudwatch.GraphWidget({
         left: [fargateCpuUtilizationMetric],
         width: 12,
