a85cbe9553866af34833dced1c6651c45c9ca954
Tan Chang Kiat <tanchangkiat@gmail.com>
2024-09-18 02:06:23 +0000

    Added a Ray Cluster and Karpenter Node Pool for GPU nodes. Added a Jupyter Notebook example for training on GPU nodes.

--- scripts/EKS/install-ray.sh
+++ scripts/EKS/install-ray.sh
@@ -2,6 +2,7 @@
 
 export RAY_VERSION="2.34.0"
 
+# Creates a node pool for Ray head
 cat <<EOF >>ray-head-node-pool.yaml
 apiVersion: karpenter.sh/v1
 kind: NodePool
@@ -55,6 +56,7 @@ spec:
     eks-cost-team: tck
 EOF
 
+# Creates a node pool for Ray workers
 cat <<EOF >>ray-worker-node-pool.yaml
 apiVersion: karpenter.sh/v1
 kind: NodePool
@@ -180,10 +182,10 @@ spec:
           operator: "Exists"
           effect: "NoSchedule"
   workerGroupSpecs:
-  - replicas: 1
-    minReplicas: 1
+  - replicas: 0
+    minReplicas: 0
     maxReplicas: 10
-    groupName: small-group
+    groupName: cpu-group
     rayStartParams: {}
     # Pod template
     template:
@@ -209,6 +211,170 @@ spec:
 EOF
 
 kubectl apply -f ray-cluster-config.yaml
+
+# Install NVIDIA device plugin for Kubernetes
+kubectl apply -f https://raw.githubusercontent.com/NVIDIA/k8s-device-plugin/v0.16.2/deployments/static/nvidia-device-plugin.yml
+
+# Creates a GPU-enabled node pool
+cat <<EOF >>ray-worker-gpu-node-pool.yaml
+apiVersion: karpenter.sh/v1
+kind: NodePool
+metadata:
+  name: ray-worker-gpu
+spec:
+  template:
+    spec:
+      requirements:
+      - key: karpenter.k8s.aws/instance-category
+        operator: In
+        values: ["g"]
+      - key: "karpenter.sh/capacity-type"
+        operator: In
+        values: ["on-demand"]
+      nodeClassRef:
+        group: karpenter.k8s.aws
+        kind: EC2NodeClass
+        name: ray-worker-gpu
+      taints:
+      - key: "nvidia.com/gpu"
+        effect: "NoSchedule"
+  disruption:
+    consolidationPolicy: WhenEmpty
+    consolidateAfter: 30s
+  limits:
+    cpu: 64
+---
+apiVersion: karpenter.k8s.aws/v1
+kind: EC2NodeClass
+metadata:
+  name: ray-worker-gpu
+spec:
+  amiFamily: "Bottlerocket"
+  role: "KarpenterNodeRole-${AWS_EKS_CLUSTER}"
+  subnetSelectorTerms:
+    - tags:
+        karpenter.sh/discovery: ${AWS_EKS_CLUSTER}
+  securityGroupSelectorTerms:
+    - tags:
+        "aws:eks:cluster-name": ${AWS_EKS_CLUSTER}
+  amiSelectorTerms:
+    - alias: bottlerocket@latest
+  blockDeviceMappings:
+    # Root device
+    - deviceName: /dev/xvda
+      ebs:
+        volumeSize: 10Gi
+        volumeType: gp3
+        encrypted: true
+    # Data device: Container resources such as images and logs
+    - deviceName: /dev/xvdb
+      ebs:
+        volumeSize: 50Gi
+        volumeType: gp3
+        encrypted: true
+  tags:
+    Name: ${AWS_EKS_CLUSTER}/karpenter/ray-worker-gpu
+    eks-cost-cluster: ${AWS_EKS_CLUSTER}
+    eks-cost-workload: ray
+    eks-cost-team: tck
+EOF
+
+kubectl apply -f ray-worker-gpu-node-pool.yaml
+
+# Creates a Ray Cluster with GPU-enabled nodes
+cat <<EOF >>ray-cluster-gpu-config.yaml
+apiVersion: ray.io/v1
+kind: RayCluster
+metadata:
+  name: raycluster-gpu
+spec:
+  rayVersion: "${RAY_VERSION}"
+  enableInTreeAutoscaling: true
+  autoscalerOptions:
+    upscalingMode: Default
+    idleTimeoutSeconds: 60
+    imagePullPolicy: IfNotPresent
+    # Optionally specify the Autoscaler container's securityContext.
+    securityContext: {}
+    env: []
+    envFrom: []
+    resources:
+      limits:
+        cpu: "500m"
+        memory: "512Mi"
+      requests:
+        cpu: "500m"
+        memory: "512Mi"
+  headGroupSpec:
+    rayStartParams:
+      # Setting "num-cpus: 0" to avoid any Ray actors or tasks being scheduled on the Ray head Pod.
+      num-cpus: "0"
+    serviceType: ClusterIP
+    template:
+      spec:
+        containers:
+        # The Ray head container
+        - name: ray-head-gpu
+          image: rayproject/ray:${RAY_VERSION}
+          ports:
+          - containerPort: 6379
+            name: gcs
+          - containerPort: 8265
+            name: dashboard
+          - containerPort: 10001
+            name: client
+          lifecycle:
+            preStop:
+              exec:
+                command: ["/bin/sh","-c","ray stop"]
+          resources:
+            limits:
+              cpu: "1"
+              memory: "2G"
+            requests:
+              cpu: "1"
+              memory: "2G"
+          env:
+            - name: RAY_enable_autoscaler_v2 # Pass env var for the autoscaler v2.
+              value: "1"
+        restartPolicy: Never # No restart to avoid reuse of pod for different ray nodes.
+        nodeSelector:
+          karpenter.sh/nodepool: ray-head
+        tolerations:
+        - key: "ray-head"
+          operator: "Exists"
+          effect: "NoSchedule"
+  workerGroupSpecs:
+  - replicas: 0
+    minReplicas: 0
+    maxReplicas: 10
+    groupName: gpu-group
+    rayStartParams: {}
+    # Pod template
+    template:
+      spec:
+        containers:
+        - name: ray-worker-gpu
+          image: rayproject/ray:${RAY_VERSION}-gpu
+          resources:
+            limits:
+              cpu: "7"
+              memory: "28Gi"
+              nvidia.com/gpu: "1"
+            requests:
+              cpu: "7"
+              memory: "28Gi"
+              nvidia.com/gpu: "1"
+        restartPolicy: Never # Never restart a pod to avoid pod reuse
+        nodeSelector:
+          karpenter.sh/nodepool: ray-worker-gpu
+        tolerations:
+        - key: "nvidia.com/gpu"
+          effect: "NoSchedule"
+          operator: "Exists"
+EOF
+
+kubectl apply -f ray-cluster-gpu-config.yaml
 
 echo ""
 echo "Installation completed. Pods may take a few minutes to start. Check the status using 'kubectl get pods'."
--- scripts/EKS/remove-ray.sh
+++ scripts/EKS/remove-ray.sh
@@ -1,12 +1,19 @@
 #!/bin/bash
 
+kubectl delete -f ray-cluster-gpu-config.yaml
+rm ray-cluster-gpu-config.yaml
+
+kubectl delete -f ray-worker-gpu-node-pool.yaml
+rm ray-worker-gpu-node-pool.yaml
+
+kubectl delete -f https://raw.githubusercontent.com/NVIDIA/k8s-device-plugin/v0.16.2/deployments/static/nvidia-device-plugin.yml
+
 kubectl delete -f ray-cluster-config.yaml
+rm ray-cluster-config.yaml
 
 helm uninstall kuberay-operator
 
 helm repo remove kuberay
-
-rm ray-cluster-config.yaml
 
 kubectl delete -f ray-worker-node-pool.yaml
 rm ray-worker-node-pool.yaml
