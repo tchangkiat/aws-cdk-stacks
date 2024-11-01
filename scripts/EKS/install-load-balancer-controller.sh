2fb74a65635d8e946b541214e0851c5b95874998
Tan Chang Kiat <tanchangkiat@gmail.com>
2024-11-01 14:43:27 +0000

    Upgraded AWS Load Balancer Controller

--- scripts/EKS/install-load-balancer-controller.sh
+++ scripts/EKS/install-load-balancer-controller.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
+export LBC_VERSION="2.10.0"
+
 eksctl utils associate-iam-oidc-provider \
     --region $AWS_REGION \
     --cluster $AWS_EKS_CLUSTER \
     --approve
 
-curl -o aws-load-balancer-controller-policy.json https://raw.githubusercontent.com/kubernetes-sigs/aws-load-balancer-controller/v2.8.1/docs/install/iam_policy.json
+curl -o aws-load-balancer-controller-policy.json "https://raw.githubusercontent.com/kubernetes-sigs/aws-load-balancer-controller/v${LBC_VERSION}/docs/install/iam_policy.json"
 
 aws iam create-policy \
  --policy-name $AWS_EKS_CLUSTER-aws-load-balancer-controller \
@@ -27,7 +29,7 @@ helm repo update
 curl -o aws-load-balancer-controller-crds.yaml https://raw.githubusercontent.com/aws/eks-charts/master/stable/aws-load-balancer-controller/crds/crds.yaml
 kubectl apply -f aws-load-balancer-controller-crds.yaml
 
-helm upgrade -i aws-load-balancer-controller eks/aws-load-balancer-controller -n kube-system --set clusterName=$AWS_EKS_CLUSTER --set serviceAccount.create=false --set serviceAccount.name=aws-load-balancer-controller \
+helm install aws-load-balancer-controller eks/aws-load-balancer-controller -n kube-system --set clusterName=$AWS_EKS_CLUSTER --set serviceAccount.create=false --set serviceAccount.name=aws-load-balancer-controller \
     --set tolerations[0].key=CriticalAddonsOnly \
     --set tolerations[0].operator=Exists \
     --set tolerations[0].effect=NoSchedule
