9f34f96a0fc6dbbab79a5f85c9ca7fe577ab6669
Tan Chang Kiat <tanchangkiat@gmail.com>
2025-04-29 02:52:28 +0000

    Removed legacy method for setting up Container Insights

--- scripts/EKS/install-container-insights.sh
+++ scripts/EKS/install-container-insights.sh
@@ -1,13 +1,4 @@
 #!/bin/bash
 
-# Legacy Method
-#ClusterName=$AWS_EKS_CLUSTER
-#RegionName=$AWS_REGION
-#FluentBitHttpPort='2020'
-#FluentBitReadFromHead='Off'
-#[[ ${FluentBitReadFromHead} = 'On' ]] && FluentBitReadFromTail='Off'|| FluentBitReadFromTail='On'
-#[[ -z ${FluentBitHttpPort} ]] && FluentBitHttpServer='Off' || FluentBitHttpServer='On'
-#curl https://raw.githubusercontent.com/tchangkiat/aws-cdk-stacks/main/assets/container-insights.yaml | sed 's/{{cluster_name}}/'${ClusterName}'/;s/{{region_name}}/'${RegionName}'/;s/{{http_server_toggle}}/"'${FluentBitHttpServer}'"/;s/{{http_server_port}}/"'${FluentBitHttpPort}'"/;s/{{read_from_head}}/"'${FluentBitReadFromHead}'"/;s/{{read_from_tail}}/"'${FluentBitReadFromTail}'"/' | kubectl apply -f - 
-
 # Documentation: https://docs.aws.amazon.com/AmazonCloudWatch/latest/monitoring/Container-Insights-setup-EKS-addon.html
 aws eks create-addon --cluster-name $AWS_EKS_CLUSTER --addon-name amazon-cloudwatch-observability
--- scripts/EKS/remove-container-insights.sh
+++ scripts/EKS/remove-container-insights.sh
@@ -1,12 +1,3 @@
 #!/bin/bash
 
-# Legacy Method
-#ClusterName=$AWS_EKS_CLUSTER
-#RegionName=$AWS_REGION
-#FluentBitHttpPort='2020'
-#FluentBitReadFromHead='Off'
-#[[ ${FluentBitReadFromHead} = 'On' ]] && FluentBitReadFromTail='Off'|| FluentBitReadFromTail='On'
-#[[ -z ${FluentBitHttpPort} ]] && FluentBitHttpServer='Off' || FluentBitHttpServer='On'
-#curl https://raw.githubusercontent.com/tchangkiat/aws-cdk-stacks/main/assets/container-insights.yaml | sed 's/{{cluster_name}}/'${ClusterName}'/;s/{{region_name}}/'${RegionName}'/;s/{{http_server_toggle}}/"'${FluentBitHttpServer}'"/;s/{{http_server_port}}/"'${FluentBitHttpPort}'"/;s/{{read_from_head}}/"'${FluentBitReadFromHead}'"/;s/{{read_from_tail}}/"'${FluentBitReadFromTail}'"/' | kubectl delete -f - 
-
 aws eks delete-addon --cluster-name $AWS_EKS_CLUSTER --addon-name amazon-cloudwatch-observability
