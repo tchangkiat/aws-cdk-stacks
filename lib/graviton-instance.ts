ace9075f3891c1a9f0773073c7eddb41f3c7b1d7
tchangkiat <tanchangkiat@gmail.com>
2025-07-14 04:02:08 +0000

    Moved commands from a bash script to EC2 user data

--- lib/graviton-instance.ts
+++ lib/graviton-instance.ts
@@ -58,8 +58,26 @@ export class GravitonInstance extends Stack {
 
     const userData = [
       "sudo yum update -y",
-      "curl -o /home/ec2-user/setup.sh https://raw.githubusercontent.com/tchangkiat/aws-cdk-stacks/main/assets/setup-graviton-instance.sh",
-      "chmod +x /home/ec2-user/setup.sh",
+      // Git
+      "sudo yum install git -y",
+      // perf
+      "sudo yum install perf -y",
+      // perl-open; see issue: https://github.com/brendangregg/FlameGraph/issues/245
+      "sudo yum install perl-open.noarch -y",
+      // Go
+      "wget -O /home/ec2-user/go.tar.gz https://go.dev/dl/go1.24.5.linux-arm64.tar.gz",
+      "sudo tar -C /usr/local -xzf /home/ec2-user/go.tar.gz",
+      "sudo echo 'export PATH=\"$PATH:/usr/local/go/bin\"' >> /home/ec2-user/.bashrc",
+      "sudo rm /home/ec2-user/go.tar.gz",
+      // APerf
+      "wget -O /home/ec2-user/aperf.tar.gz https://github.com/aws/aperf/releases/download/v0.1.15-alpha/aperf-v0.1.15-alpha-aarch64.tar.gz",
+      "sudo tar -C /home/ec2-user/ -xzf /home/ec2-user/aperf.tar.gz",
+      "sudo mv /home/ec2-user/aperf-v0.1.15-alpha-aarch64 /home/ec2-user/aperf",
+      "sudo rm /home/ec2-user/aperf.tar.gz",
+      // FlameGraph
+      "git clone https://github.com/brendangregg/FlameGraph /home/ec2-user/FlameGraph",
+      // Example Golang application
+      "git clone https://github.com/tchangkiat/go-gin /home/ec2-user/go-gin",
     ];
     instance.addUserData(userData.join("\n"));
 
