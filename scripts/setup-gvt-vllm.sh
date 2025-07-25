641a907fd56ddcdacce8bdc9d86c48f9ea2d8401
tchangkiat <tanchangkiat@gmail.com>
2025-07-25 06:41:10 +0000

    Fixed a vLLM compilation error

--- scripts/setup-gvt-vllm.sh
+++ scripts/setup-gvt-vllm.sh
@@ -9,12 +9,12 @@ source .venv/bin/activate
 sudo apt-get update -y
 sudo apt-get install -y --no-install-recommends ccache git curl wget ca-certificates gcc-12 g++-12 libtcmalloc-minimal4 libnuma-dev ffmpeg libsm6 libxext6 libgl1 jq lsof
 sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-12 10 --slave /usr/bin/g++ g++ /usr/bin/g++-12
-export CCACHE_DIR=/ubuntu/.cache/ccache
+export CCACHE_DIR=$HOME/.cache/ccache
 export CMAKE_CXX_COMPILER_LAUNCHER=ccache
-sudo echo 'export CCACHE_DIR=/ubuntu/.cache/ccache' | tee -a /home/ubuntu/.bashrc /home/ubuntu/.zshrc
-sudo echo 'export CMAKE_CXX_COMPILER_LAUNCHER=ccache' | tee -a /home/ubuntu/.bashrc /home/ubuntu/.zshrc
+sudo echo 'export CCACHE_DIR=$HOME/.cache/ccache' | tee -a $HOME/.bashrc $HOME/.zshrc
+sudo echo 'export CMAKE_CXX_COMPILER_LAUNCHER=ccache' | tee -a $HOME/.bashrc $HOME/.zshrc
 export LD_PRELOAD="/usr/lib/aarch64-linux-gnu/libtcmalloc_minimal.so.4"
-sudo echo 'export LD_PRELOAD="/usr/lib/aarch64-linux-gnu/libtcmalloc_minimal.so.4"' | tee -a /home/ubuntu/.bashrc /home/ubuntu/.zshrc
+sudo echo 'export LD_PRELOAD="/usr/lib/aarch64-linux-gnu/libtcmalloc_minimal.so.4"' | tee -a $HOME/.bashrc $HOME/.zshrc
 # Clone vLLM project
 git clone https://github.com/vllm-project/vllm.git vllm_source
 cd vllm_source
@@ -23,7 +23,6 @@ sudo apt-get install python3-pip -y
 pip install -v -r requirements/cpu-build.txt --extra-index-url https://download.pytorch.org/whl/cpu
 pip install -v -r requirements/cpu.txt --extra-index-url https://download.pytorch.org/whl/cpu
 # Install vLLM
-# python3 setup.py install
 python3 setup.py bdist_wheel
 pip install dist/*.whl
 rm -rf dist
@@ -32,7 +31,7 @@ export VLLM_TARGET_DEVICE=cpu
 export VLLM_CPU_KVCACHE_SPACE=40
 export VLLM_CPU_OMP_THREADS_BIND=0-30
 export VLLM_CPU_DISABLE_AVX512="true"
-sudo echo 'export VLLM_TARGET_DEVICE=cpu' | tee -a /home/ubuntu/.bashrc /home/ubuntu/.zshrc
-sudo echo 'export VLLM_CPU_KVCACHE_SPACE=40' | tee -a /home/ubuntu/.bashrc /home/ubuntu/.zshrc
-sudo echo 'export VLLM_CPU_OMP_THREADS_BIND=0-30' | tee -a /home/ubuntu/.bashrc /home/ubuntu/.zshrc
-sudo echo 'export VLLM_CPU_DISABLE_AVX512="true"' | tee -a /home/ubuntu/.bashrc /home/ubuntu/.zshrc
+sudo echo 'export VLLM_TARGET_DEVICE=cpu' | tee -a $HOME/.bashrc $HOME/.zshrc
+sudo echo 'export VLLM_CPU_KVCACHE_SPACE=40' | tee -a $HOME/.bashrc $HOME/.zshrc
+sudo echo 'export VLLM_CPU_OMP_THREADS_BIND=0-30' | tee -a $HOME/.bashrc $HOME/.zshrc
+sudo echo 'export VLLM_CPU_DISABLE_AVX512="true"' | tee -a $HOME/.bashrc $HOME/.zshrc
